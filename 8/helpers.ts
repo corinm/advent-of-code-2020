91afcfde56a164da19685105f5aec015eb177b27
Corin Mulliss <corin.mulliss@gmail.com>
2020-12-09 14:00:52 +0000

    Solved Day 8

--- 8/helpers.ts
+++ 8/helpers.ts
@@ -22,3 +22,57 @@ const parseLine = (line: string): Instruction => {
   const [op, argStr] = line.split(" ");
   return { op: op as Op, arg: Number(argStr) };
 };
+
+export const solvePart1 = (instructions: Instruction[]): number => {
+  let pointer = 0;
+  let accumulator = 0;
+  let running = true;
+  let instructionsRun = {};
+
+  while (running) {
+    if (instructionsRun[pointer]) {
+      return accumulator;
+    }
+    if (!instructions[pointer]) {
+      return accumulator;
+    }
+
+    const { op, arg } = instructions[pointer];
+    instructionsRun[pointer] = true;
+
+    if (op === "nop") {
+      pointer++;
+    } else if (op === "acc") {
+      accumulator += arg;
+      pointer++;
+    } else if (op === "jmp") {
+      pointer += arg;
+    }
+  }
+};
+
+export const runsToInfiniteLoop = (instructions: Instruction[]): boolean => {
+  let pointer = 0;
+  let running = true;
+  let instructionsRun: Set<number> = new Set();
+
+  while (running) {
+    if (instructionsRun.has(pointer)) {
+      return true;
+    }
+    if (!instructions[pointer]) {
+      return false;
+    }
+
+    const { op, arg } = instructions[pointer];
+    instructionsRun.add(pointer);
+
+    if (op === "nop") {
+      pointer++;
+    } else if (op === "acc") {
+      pointer++;
+    } else if (op === "jmp") {
+      pointer += arg;
+    }
+  }
+};
--- 8/index.ts
+++ 8/index.ts
@@ -1,18 +1,52 @@
-import { readData, Instruction } from "./helpers";
+import {
+  readData,
+  Instruction,
+  runsToInfiniteLoop,
+  solvePart1,
+} from "./helpers";
 
-const solve = (instructions: Instruction[]): number => {
+const findInstructionThatIsCorrupted = (
+  instructions: Instruction[]
+): string => {
   let pointer = 0;
   let accumulator = 0;
   let running = true;
-  let instructionsRun = {};
+  let instructionsRun = new Set();
+  let instructionsRunInOrder: number[] = [];
 
   while (running) {
-    if (instructionsRun[pointer]) {
-      return accumulator;
+    if (instructionsRun.has(pointer)) {
+      console.log("Infinite loop detected at:", pointer);
+
+      const instructionsInReverse = [...instructionsRunInOrder].reverse();
+
+      let corruptedInstruction: string | undefined = undefined;
+
+      for (const instruction in instructionsInReverse) {
+        const tempInstructions = instructions.map((inst) => ({ ...inst }));
+
+        if (tempInstructions[instruction].op === "nop") {
+          tempInstructions[instruction].op = "jmp";
+          if (!runsToInfiniteLoop(tempInstructions)) {
+            corruptedInstruction = instruction;
+            break;
+          }
+        }
+        if (tempInstructions[instruction].op === "jmp") {
+          tempInstructions[instruction].op = "nop";
+          if (!runsToInfiniteLoop(tempInstructions)) {
+            corruptedInstruction = instruction;
+            break;
+          }
+        }
+      }
+
+      return corruptedInstruction;
     }
 
     const { op, arg } = instructions[pointer];
-    instructionsRun[pointer] = true;
+    instructionsRun.add(pointer);
+    instructionsRunInOrder.push(pointer);
 
     if (op === "nop") {
       pointer++;
@@ -25,10 +59,22 @@ const solve = (instructions: Instruction[]): number => {
   }
 };
 
+const solvePart2 = (instructions: Instruction[]) => {
+  const corruptedInstruction = findInstructionThatIsCorrupted(instructions);
+  const correctedInstructions = instructions.map(
+    (inst, i): Instruction =>
+      i.toString() === corruptedInstruction
+        ? { op: inst.op === "jmp" ? "nop" : "jmp", arg: inst.arg }
+        : { ...inst }
+  );
+  const correctedAnswer = solvePart1(correctedInstructions);
+  return correctedAnswer;
+};
+
 const main = async () => {
   const data = await readData();
-  const result = solve(data);
-  console.log(result);
+  console.log("Part 1:", solvePart1(data));
+  console.log("Part 2:", solvePart2(data));
 };
 
 main();
